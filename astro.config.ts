d020832b62b446322ce14950046d701e451c98ac
yaoshiu <yaoshiu@qq.com>
2024-09-07 07:42:16 +0800

    feat: add Vercel adapter and enable web analytics and speed insights
    - Added `@astrojs/vercel` adapter for deployment on Vercel.
    - Enabled Vercel Web Analytics and Speed Insights in the Astro
    configuration.
    - Updated `package.json` to include dependencies for Vercel integration.
    - Injected Vercel Speed Insights script into the Base layout.

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,15 @@
 import solid from '@astrojs/solid-js';
 import { defineConfig } from 'astro/config';
 import unocss from 'unocss/astro';
+import vercel from '@astrojs/vercel/static';
 
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
   integrations: [
     solid(),
     unocss({
