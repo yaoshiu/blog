093e0ac21cf2007e5cc340bf70600b0e2450989a
yaoshiu <yaoshiu@qq.com>
2024-09-11 23:20:12 +0800

    chore: update dependencies and enhance component functionality
    - Dependencies:
      - Updated various dependencies in `package.json`, including
    `@astrojs/vercel`, `astro`, `typescript`, `vercel`, and others to their
    latest versions.
      - Added new dependencies: `reading-time`, `dayjs`, and `motion`.
    
    - Components:
      - Created a new `SlideIn.astro` component for slide-in animations on
    page load.
      - Enhanced `Footer.astro` and `Head.astro` components with updated
    styles and improved functionality.
      - Modified `ThemeSwitcher.tsx` to utilize Solid.js reactivity for
    theme switching.
      - Updated `TypeWriter.tsx` component to refine typewriting effect
    handling.
    
    - Layout and Pages:
      - Improved the `Base.astro` layout with better structure and
    transitions.
      - Added a new custom 404 page (`404.astro`).
      - Applied slide-in animations to elements on the `index.astro` page.
    
    - .gitattributes:
      - Added a new `.gitattributes` file to handle binary files correctly.

--- src/components/solid/ThemeSwitcher.tsx
+++ src/components/solid/ThemeSwitcher.tsx
@@ -1,18 +1,25 @@
-function toggleDark() {
-  document.documentElement.classList.toggle('dark');
-}
+import { createEffect, createSignal, onMount } from 'solid-js';
 
-export const ThemeSwitcher = () => {
+export default function ThemeSwitcher() {
+  const [dark, setDark] = createSignal(false);
+
+  onMount(() => {
+    setDark(localStorage.getItem('theme') === 'dark');
+
+    createEffect(() => {
+      document.documentElement.classList.toggle('dark', dark());
+      window.localStorage.setItem('theme', dark() ? 'dark' : 'light');
+    });
+  });
+
   return (
     <button
       type="button"
-      onClick={toggleDark}
-      aria-label="Toggle dark/light mode"
+      aria-label={`Toggle ${dark() ? 'light' : 'dark'} mode`}
       class="bg-transparent"
+      onClick={() => setDark((dark) => !dark)}
     >
       <svg class="i-pixelarticons:sun-alt dark:i-pixelarticons:moon" />
     </button>
   );
-};
-
-export default ThemeSwitcher;
+}
