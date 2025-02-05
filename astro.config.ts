2c1e682b2c82db594abf866e2a8e2eeef7d5335e
Fay Ash <yaoshiu@qq.com>
2025-02-06 01:17:48 +0800

    fix(config): switch Vercel adapter to static version
    Changed the Vercel adapter from '@astrojs/vercel' to '@astrojs/vercel-static' to better support static site deployment. This update ensures compatibility with the intended deployment configuration for static sites.

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import solid from '@astrojs/solid-js';
-import vercel from '@astrojs/vercel';
+import vercelStatic from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 import unocss from 'unocss/astro';
 import lastModified from './lastModified';
@@ -21,7 +21,7 @@ const LOCAL = 'http://localhost:4321';
 // https://astro.build/config
 export default defineConfig({
   site: process.env.SITE ?? LOCAL,
-  adapter: vercel({
+  adapter: vercelStatic({
     webAnalytics: {
       enabled: true,
     },
